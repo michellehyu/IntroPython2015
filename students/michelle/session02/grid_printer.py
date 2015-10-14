2af2cbb3d9c747cd21b84a5fbea222f0ddba6434
Michelle Yu <michellehyu7@gmail.com>
2015-10-13 20:35:56 -0700

    fix the sizing issue

--- students/michelle/session02/grid_printer.py
+++ students/michelle/session02/grid_printer.py
@@ -5,7 +5,7 @@ def print_grid(n):
 	border = '+ ' + minuses + '+ ' + minuses + '+' + '\n'
 	spaces = '  '* gaps
 	middle = '| ' + spaces + '| ' + spaces + '| ' + '\n'
-	grid = border + middle * 4 + border + middle * 4 + border
+	grid = border + middle * gaps + border + middle * gaps + border
 	print (grid)
 
-print_grid(11)
+print_grid(12)
